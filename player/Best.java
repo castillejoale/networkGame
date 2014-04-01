359389148868f6131d10e102b845c98a5518e69b
Shir Nehama <chocmoose5@Shir-Nehamas-MacBook-Pro.local>
2014-03-31 18:51:55 -0700

    Shir pushes the Best class with style

--- player/Best.java
+++ player/Best.java
@@ -15,18 +15,35 @@ public class Best {
 	protected Best() { 
 	}
 	
+	/**
+	 * setMove sets the move 
+	 * 
+	 * @param m is a move. It needs to be a valid move for the current gameboard and player
+	 */
 	protected void setMove(Move m){
 		this.move = m; 
 	}
 	
+	/**
+	 * getMove() gets the Move stored in the Best 
+	 * @return the move
+	 */
 	protected Move getMove(){ 
 		return this.move; 
 	}
 	
+	/**
+	 * setScore() sets the score associated with the move 
+	 * @param s is a double score assigned by the evaluation function
+	 */
 	protected void setScore(double s){
 		this.score = s;
 	}
 	
+	/**
+	 * getScore() gets the score assigned to the move
+	 * @return a double value that represents the score of a move 
+	 */
 	protected double getScore(){
 		return this.score;
 	}
